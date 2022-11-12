eb05783e75502df99d515a893e4159c5d98c67fe
Polina Balova <polina77728@yandex.ru>
2022-11-12 15:17:08 +0300

    Задача 1 - найти мах и мin число

---
+++ 001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int numberA = new Random().Next(-100, 100);
+int numberB = new Random().Next(-100, 100);
+
+if (numberA > numberB)
+{
+    Console.WriteLine("a = " + numberA + ", b = " + numberB);
+    Console.WriteLine("max -> " + numberA + ", min -> " + numberB);
+}
+
+else if (numberA < numberB)
+{
+    Console.WriteLine("a = " + numberA + ", b = " + numberB);
+    Console.WriteLine("max -> " + numberB + ", min -> " + numberA);
+}
+
+else
+{
+    Console.WriteLine("a = " + numberA + ", b = " + numberB);
+    Console.WriteLine("эти два числа равны");
+}
