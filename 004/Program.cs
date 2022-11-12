f3999c254376659924b1b32221f0026051745a92
Polina Balova <polina77728@yandex.ru>
2022-11-12 16:12:48 +0300

    Задача 4 - показывает все числа, а надо только четные

---
+++ 004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write ("Введите число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+
+if (number < 1)
+{
+    Console.Write ("ОШИБКА !!!");
+}
+
+else
+{
+    while (num <= number)
+    {
+        Console.Write(num + " ");
+        num ++;
+    }
+}
