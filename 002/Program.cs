1e9a9388cf59823854cfc9bca9735a5a9f6b048e
Polina Balova <polina77728@yandex.ru>
2022-11-12 15:41:20 +0300

    Задача 2 - найти маx из трех чисел

---
+++ 002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int numberA = new Random().Next(1, 100);
+int numberB = new Random().Next(1, 100);
+int numberC = new Random().Next(1, 100);
+int max = 0;
+
+if (max < numberA)
+{
+    max = numberA;
+
+
+    if (max < numberB)
+    {
+        max = numberB;
+    }
+
+    else if (max < numberC)
+    {
+        max = numberC;
+    }
+}
+Console.WriteLine("a = " + numberA + ", b = " + numberB + ", c = " + numberC);
+Console.WriteLine("max -> " + max);
